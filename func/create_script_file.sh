33c535280d5d734f629bd7f681479da1edd20b0b
budRich <robstenklippa@gmail.com>
2022-07-28 19:33:47 +0200

    [FIX] test for existing of ~/.bashrc before cat

--- func/create_script_file.sh
+++ func/create_script_file.sh
@@ -69,7 +69,8 @@ create_script_file() {
     # to update the title to USER@HOST:PWD
     # which overwrite i3terms setting of the title
     [[ $_shell =~ bash && ${conf[auto_set_title],,} =~ true|yes ]] \
-      && echo " --rcfile <(echo unset PROMPT_COMMAND ; cat ~/.bashrc)"
+      && echo ' --rcfile <( echo unset PROMPT_COMMAND
+                            [[ -f ~/.bashrc ]] && cat ~/.bashrc )'
   fi >> "$_temp_file"
 
   chmod +x "$_temp_file"
